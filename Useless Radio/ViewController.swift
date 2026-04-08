b8811bceffbaeda9fe07b2fb856ed21907363039
Ethan Fountain <ethanfountain03@gmail.com>
2026-04-08 17:37:10 -0400

    Initial Commit

---
+++ Useless Radio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Useless Radio
+//
+//  Created by Ethan  Fountain on 08/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
